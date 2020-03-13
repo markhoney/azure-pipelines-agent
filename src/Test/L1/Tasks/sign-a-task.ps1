4b1e08e78ad3b11a94625e497bbc54da66ef9860
Stephen Franceschelli <stephenmichaelf@gmail.com>
2020-03-13 11:19:21 -0400

    Zipping task working.

--- src/Test/L1/Tasks/sign-a-task.ps1
+++ src/Test/L1/Tasks/sign-a-task.ps1
@@ -1,13 +1,52 @@
-# Sign a task
-# Run this with:
-# sign-a-task.ps1 .\MyTask
-# It will output MyTask.Zip at the same level as the MyTask folder
+# https://docs.microsoft.com/en-us/nuget/create-packages/sign-a-package#create-a-test-certificate
+
+# e.g. - D:\github\vsts-agent\src\Test\L1\Tasks\SignedTaskCertA
+$taskPath = Resolve-Path $args[0]
+
+Write-Host "Zipping task folder "
+
+# e.g. - D:\github\vsts-agent\src\Test\L1\Tasks
+$parentDirectory = [System.IO.Path]::GetDirectoryName($taskPath)
+
+# e.g. - SignedTaskCertA
+$taskDirectoryName = Split-Path $taskPath -Leaf
+
+# e.g - D:\github\vsts-agent\src\Test\L1\Tasks\SignedTaskCertA.zip
+$zipPath = $parentDirectory + "\" + $taskDirectoryName + ".zip"
 
 
+# Write-Host ($Files = @(Get-ChildItem -Path $taskPath))
+$filesInFolder = (Get-ChildItem $taskPath -Recurse).fullname
+
+# $compress = @{
+#     LiteralPath = $filesInFolder 
+#     DestinationPath = $zipPath
+# }
+Compress-Archive -LiteralPath $filesInFolder -DestinationPath $zipPath -Force
+#Compress-Archive -Path $taskPath -DestinationPath $zipPath
+
+Write-Host "Generating signature"
+
+# New-SelfSignedCertificate -Subject "CN=NuGet Test Developer, OU=Use for testing purposes ONLY" `
+#                           -FriendlyName "NuGetTestDeveloper" `
+#                           -Type CodeSigning `
+#                           -KeyUsage DigitalSignature `
+#                           -KeyLength 2048 `
+#                           -KeyAlgorithm RSA `
+#                           -HashAlgorithm SHA256 `
+#                           -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" `
+#                           -CertStoreLocation "Cert:\CurrentUser\My"
+
+# Write-Host "Signature generated"
+
+# Print out fingerprint
 
 
+# Write-Host "Signing task"
 
-
+# rename
+# sign
+# rename back
 
 
 
